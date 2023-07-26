634510eabc7258c915f7e5c6f1f06a927f6214ed
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2023-07-26 14:10:41 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div className="h-[100vh] w-[100vw] flex  justify-center items-center gap-2 bg-gray-300">
-        <a href="https://twitter.com/Viisactive" className=" w-[20%] text-2xl transition-all delay-70 duration-200 ease flex  justify-evenly items-center cursor-pointer hover:scale-[1.1] hover:text-[#1DA1F2] "> <span><BsTwitter className="text-4xl text-[#1DA1F2]"/></span>{string}</a>
+        <a href="https://twitter.com/Viisactive" className=" w-[80%] sm:w-[45%] md:w-[37%] lg:w-[27%] xl:w-[23%] 2xl:w-[20%] text-2xl transition-all delay-70 duration-200 ease flex  justify-evenly items-center cursor-pointer hover:scale-[1.1] hover:text-[#1DA1F2] "> <span><BsTwitter className="text-4xl text-[#1DA1F2]"/></span>{string}</a>
       </div>
     </>
   );
